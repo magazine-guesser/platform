ef4c8b34f63a02c025b8f986c1c80d47b0ce6d20
Dominik Schmidt <schmidt.dominik@uni-ulm.de>
2026-04-22 17:54:07 +0200

    feat: AppStack stack wide destroy policy

--- cdk/lib/app-stack.ts
+++ cdk/lib/app-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import { aws_dynamodb as dynamodb, aws_ecs as ecs, aws_ec2 as ec2, aws_ecr as ecr } from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb, aws_ecs as ecs, aws_ec2 as ec2, aws_ecr as ecr, Aspects } from 'aws-cdk-lib';
+import { DestroyAll } from './aspects';
 
 export class AppStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,7 +13,6 @@ export class AppStack extends cdk.Stack {
             partitionKey: { name: 'date', type: dynamodb.AttributeType.STRING },
             sortKey: { name: 'nr', type: dynamodb.AttributeType.NUMBER },
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-            removalPolicy: cdk.RemovalPolicy.DESTROY
         });
 
         const vpc = ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
@@ -31,6 +31,7 @@ export class AppStack extends cdk.Stack {
         const sg = new ec2.SecurityGroup(this, 'TaskSg', { vpc });
         sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3000)); //TODO: restrict once ALB is set up
 
+        Aspects.of(this).add(new DestroyAll());
         new cdk.CfnOutput(this, 'ClusterName', { value: cluster.clusterName });
         new cdk.CfnOutput(this, 'TaskDefArn', { value: taskDef.taskDefinitionArn });
         new cdk.CfnOutput(this, 'SecurityGroupId', { value: sg.securityGroupId });

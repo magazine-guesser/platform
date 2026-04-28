4759873c2482351cb0705659919e388ddab26e85
Dominik Schmidt <schmidt.dominik@uni-ulm.de>
2026-04-28 04:45:36 +0200

    test: added infra stack tests

--- cdk/test/infra/infra-stack.test.ts
+++ cdk/test/infra/infra-stack.test.ts
@@ -1,14 +1,42 @@
 import * as cdk from 'aws-cdk-lib/core'
 import { Template } from 'aws-cdk-lib/assertions'
 import { InfraStack } from '../../lib/infra-stack'
+import { aws_certificatemanager as acm } from 'aws-cdk-lib'
 
-describe('InfraStack', () => {
+const ACCOUNT = '123456789012'
+const REGION = 'eu-central-1'
+const DOMAIN = 'magazineguessr.com'
+
+const hostedZoneContext = {
+  [`hosted-zone:account=${ACCOUNT}:domainName=${DOMAIN}:region=${REGION}`]: {
+    Id: '/hostedzone/TESTZONEID',
+    Name: `${DOMAIN}.`,
+  },
+}
+
+const buildTemplate = () => {
+  const app = new cdk.App({ context: hostedZoneContext })
+
+  // mock cert on a helper stack, mirrors CertStack in production
+  const helperStack = new cdk.Stack(app, 'HelperStack')
+  const mockCert = acm.Certificate.fromCertificateArn(
+    helperStack,
+    'MockCert',
+    `arn:aws:acm:us-east-1:${ACCOUNT}:certificate/mock-cert-id`
+  )
+
+  const stack = new InfraStack(app, 'TestInfraStack', {
+    env: { account: ACCOUNT, region: REGION },
+    certificate: mockCert,
+  })
+
+  return Template.fromStack(stack)
+}
+
+describe('InfraStack: storage', () => {
   let template: Template
-
   beforeEach(() => {
-    const app = new cdk.App()
-    const stack = new InfraStack(app, 'TestInfraStack')
-    template = Template.fromStack(stack)
+    template = buildTemplate()
   })
 
   test('ECR repository is created with the correct name', () => {
@@ -23,6 +51,67 @@ describe('InfraStack', () => {
         { AttributeName: 'date', KeyType: 'HASH' },
         { AttributeName: 'nr', KeyType: 'RANGE' },
       ],
+    })
+  })
+
+  test('S3 bucket blocks all public access', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+    })
+  })
+
+  test('Secrets Manager secret is created for admin key', () => {
+    template.hasResourceProperties('AWS::SecretsManager::Secret', {
+      Name: 'admin-key',
+    })
+  })
+})
+
+describe('InfraStack: CloudFront', () => {
+  let template: Template
+  beforeEach(() => {
+    template = buildTemplate()
+  })
+
+  test('CloudFront distribution serves index.html as default root', () => {
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        DefaultRootObject: 'index.html',
+      },
+    })
+  })
+
+  test('CloudFront redirects HTTP to HTTPS', () => {
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        DefaultCacheBehavior: {
+          ViewerProtocolPolicy: 'redirect-to-https',
+        },
+      },
+    })
+  })
+
+  test('CloudFront returns index.html on 403 for React Router support', () => {
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        CustomErrorResponses: [
+          {
+            ErrorCode: 403,
+            ResponseCode: 200,
+            ResponsePagePath: '/index.html',
+          },
+          {
+            ErrorCode: 404,
+            ResponseCode: 200,
+            ResponsePagePath: '/index.html'
+          }
+        ],
+      },
     })
   })
 })
